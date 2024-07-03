0d56dda9182ba919fdc5439b75290e6eadef0844
DIPANKAR MANDAL <dipankar0535@gmail.com>
2024-07-03 22:42:17 +0530

    Update RemoveDuplicates.py

--- leetcode/RemoveDuplicates.py
+++ leetcode/RemoveDuplicates.py
@@ -1,3 +1,29 @@
+'''
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int val = ...; // Value to remove
+int[] expectedNums = [...]; // The expected answer with correct length.
+                            // It is sorted with no values equaling val.
+
+int k = removeElement(nums, val); // Calls your implementation
+
+assert k == expectedNums.length;
+sort(nums, 0, k); // Sort the first k elements of nums
+for (int i = 0; i < actualLength; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+'''
+''' My approach'''
 class Solution(object):
     def removeElement(self, nums, val):
         """
@@ -5,15 +31,34 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        nums2 = []
+        stackList = []
+        
+        for index,i in enumerate(nums):
+            if i == val:
+                stackList.append(index)
+        count = 0
+        for index,i in enumerate(stackList):
+            if index == 0:
+                nums.pop(i)
+            else:
+                nums.pop(i-count)
+            count +=1
+        length = len(nums)
+        
+        return length
 
-        val_indexes = nums.index(val)
-        print()
 
-        # for num in nums:
-        #     if num == val:
+''' Best approach'''
+class Solution(object):
+    def removeElement(self, nums, val):
+        j = 0
 
-    
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+
+        return j 
 
 
 nums = [3,2,2,3]
