07ee6621e9ff9600fcb5552d05a5eaa6b6333039
DIPANKAR MANDAL <dipankar0535@gmail.com>
2024-07-03 22:31:32 +0530

    submit

---
+++ leetcode/RemoveDuplicates.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        nums2 = []
+
+        val_indexes = nums.index(val)
+        print()
+
+        # for num in nums:
+        #     if num == val:
+
+    
+
+
+nums = [3,2,2,3]
+A = Solution()
+print(A.removeElement(nums,3))
