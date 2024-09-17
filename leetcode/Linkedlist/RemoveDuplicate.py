0c15c935833621abbea9e91c888dea4e4c688091
Dipankar Mandal <dipankar0535@gmail.com>
2024-09-18 01:30:52 +0530

    Done

--- leetcode/Linkedlist/RemoveDuplicate.py
+++ leetcode/Linkedlist/RemoveDuplicate.py
@@ -28,15 +28,58 @@ Answer:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 from typing import Optional
 
 class ListNode:
+
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-
 class Solution:
+
+    head: ListNode
+
+    def __init__(self)->None:
+        self.head = None
+
+    def MakeChain(self,llList:list)->None:
+        self.head = ListNode(llList[0])
+        current = self.head
+        for i in llList[1:]:
+            newNode = ListNode(i)
+            current.next = newNode
+            current = current.next
+        return self.head
+
+    def PrintLL(self):
+        current = self.head
+        while current:
+            print(current.val)
+            current = current.next
+
+
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         spareList = []
-        if head and head.next!= None:
+
+        if head:
+            current = head
+            beforecurrent = head
+            while current:
+                if current.val not in spareList:
+                    spareList.append(current.val)
+                    beforecurrent = current
+                else:
+                    beforecurrent.next = current.next
+                current = current.next
+
+        return  head
+
+
+A = Solution()
+head = A.MakeChain([1,1,2])
+A.PrintLL()
+A.deleteDuplicates(head)
+print("later data:")
+A.PrintLL()
