d1dfc972e2e89b91e579873a5ee7f1857dd5a22a
Dipankar Mandal <dipankar0535@gmail.com>
2024-10-02 21:30:52 +0530

    clearing the data ..

--- leetcode/MergeSortArray.py
+++ leetcode/MergeSortArray.py
@@ -5,7 +5,7 @@ Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
 The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
- 
+
 
 Example 1:
 
@@ -36,23 +36,7 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        print("Before nums1", nums1)
-        if m == 0:
-            nums1 = nums2
-        else:
-            for i in range(n):
-                if i == 0 : j = i
-                while 1:
-                    if nums1[j] < nums2[i] and j<=m:
-                        j+=1
-                    else:
-                        break
-                nums1.insert(j,nums2[i])
-                print(nums1)
-        print("Afternums1:,",nums1)
+
 
 
 a = Solution()
