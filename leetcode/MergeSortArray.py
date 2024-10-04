13603ea5f96b14f2ecb801ee8c6449b398eb4100
Dipankar Mandal <dipankar0535@gmail.com>
2024-10-04 19:42:51 +0530

    ..

--- leetcode/MergeSortArray.py
+++ leetcode/MergeSortArray.py
@@ -71,7 +71,9 @@ class Solution:
 
 a = Solution()
 # a.merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
-a.merge(nums1 = [4,5,6,0,0,0], m = 3, nums2 = [1,2,3], n = 3)
+# a.merge(nums1 = [4,5,6,0,0,0], m = 3, nums2 = [1,2,3], n = 3)
+a.merge(nums1 = [4,5,6,0,0,0,0,0,0], m = 3, nums2 = [1,2,3,5,8,13], n = 6)
+
 
 
 
